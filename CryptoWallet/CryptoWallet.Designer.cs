24dbe8e630f89cb349f3e4d6c58c44ab8b1989ec
sh3dwin <kiril.avramoski@students.finki.ukim.mk>
2021-08-19 16:11:24 +0200

    Add project files.

---
+++ CryptoWallet/CryptoWallet.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CryptoWallet
+{
+    partial class CryptoWallet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.logInButton = new System.Windows.Forms.Button();
+            this.accountCreationLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.ForeColor = System.Drawing.Color.Purple;
+            this.loginLabel.Location = new System.Drawing.Point(321, 100);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(170, 31);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "CryptoWallet";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(271, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(271, 201);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(265, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(268, 146);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(58, 13);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(268, 185);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(271, 250);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(265, 26);
+            this.logInButton.TabIndex = 5;
+            this.logInButton.Text = "LOG IN";
+            this.logInButton.UseVisualStyleBackColor = true;
+            // 
+            // accountCreationLinkLabel
+            // 
+            this.accountCreationLinkLabel.AutoSize = true;
+            this.accountCreationLinkLabel.Location = new System.Drawing.Point(359, 289);
+            this.accountCreationLinkLabel.Name = "accountCreationLinkLabel";
+            this.accountCreationLinkLabel.Size = new System.Drawing.Size(96, 13);
+            this.accountCreationLinkLabel.TabIndex = 6;
+            this.accountCreationLinkLabel.TabStop = true;
+            this.accountCreationLinkLabel.Text = "Create an Account";
+            // 
+            // CryptoWallet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.accountCreationLinkLabel);
+            this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loginLabel);
+            this.Name = "CryptoWallet";
+            this.Text = "CryptoWallet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.LinkLabel accountCreationLinkLabel;
+    }
+}
+
